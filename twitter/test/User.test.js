08e1596a5cc8ed8a1348dfad9de3ecaa469c3f75
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-26 16:04:14 -0500

    Paso 8: Corregir unit test para Create User

--- twitter/test/User.test.js
+++ twitter/test/User.test.js
@@ -1,15 +1,17 @@
 const User = require('../app/models/User');
 
-describe('Create an User Object', () => {
-    // Invocar el código para usar en la app
-    const user = new User(1,"GabrielaEsquivel", "Andrea Gabriela", "Biografía", "dateCreated", "lastUpdated")
+describe('Unit Tests for User Class', () => {
+    test('Create an User Object', () => {
+        // Invocar el código para usar en la app
+        const user = new User(1, "GabrielaEsquivel", "Andrea Gabriela", "Biografía", "dateCreated", "lastUpdated")
 
-    // Validación de los resultados del código:
-    expect(user.id),toBe(1);
-    expect(user.username).toBe("GabrielaEsquivel");
-    expect(user.name).toBe("Andrea Gabriela");
-    expect(user.bio).toBe("Biografía");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+        // Validación de los resultados del código:
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("GabrielaEsquivel");
+        expect(user.name).toBe("Andrea Gabriela");
+        expect(user.bio).toBe("Biografía");
+        expect(user.dateCreated).toBe("dateCreated");
+        expect(user.lastUpdated).toBe("lastUpdated");
+    })
 
 })
