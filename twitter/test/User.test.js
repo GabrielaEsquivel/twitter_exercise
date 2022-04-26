3a4271120d5b7942ab3e85656942ef2a3505e79b
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-26 16:07:16 -0500

    Paso 9: DateCreated y LastUpdated con contenido de fecha

--- twitter/test/User.test.js
+++ twitter/test/User.test.js
@@ -3,15 +3,15 @@ const User = require('../app/models/User');
 describe('Unit Tests for User Class', () => {
     test('Create an User Object', () => {
         // Invocar el código para usar en la app
-        const user = new User(1, "GabrielaEsquivel", "Andrea Gabriela", "Biografía", "dateCreated", "lastUpdated")
+        const user = new User(1, "GabrielaEsquivel", "Andrea Gabriela", "Biografía")
 
         // Validación de los resultados del código:
         expect(user.id).toBe(1);
         expect(user.username).toBe("GabrielaEsquivel");
         expect(user.name).toBe("Andrea Gabriela");
         expect(user.bio).toBe("Biografía");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     })
 
 })
