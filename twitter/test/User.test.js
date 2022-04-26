a6cc33ec20dca1408733950b58f15c7513666d7f
Andrea <andrea.rueda-ext@dominion-global.com>
2022-04-26 16:01:54 -0500

    Paso 7: Agregar clase User

--- twitter/test/User.test.js
+++ twitter/test/User.test.js
@@ -2,11 +2,12 @@ const User = require('../app/models/User');
 
 describe('Create an User Object', () => {
     // Invocar el código para usar en la app
-    const user = new User(1, "Andrea Gabriela", "Biografía", "dateCreated", "lastUpdated")
+    const user = new User(1,"GabrielaEsquivel", "Andrea Gabriela", "Biografía", "dateCreated", "lastUpdated")
 
     // Validación de los resultados del código:
     expect(user.id),toBe(1);
-    expect(user.username).toBe("Andrea Gabriela");
+    expect(user.username).toBe("GabrielaEsquivel");
+    expect(user.name).toBe("Andrea Gabriela");
     expect(user.bio).toBe("Biografía");
     expect(user.dateCreated).toBe("dateCreated");
     expect(user.lastUpdated).toBe("lastUpdated");
